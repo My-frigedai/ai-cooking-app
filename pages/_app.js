40bd04ef95dd3ecf6b5644c9e168595c7f22a3ab
my-frigedai <auos.05.04-conan@au.com>
2025-09-05 17:05:33 +0900

    fix: remove TypeScript syntax from _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
-import type { AppProps } from 'next/app';
 import '../pages/styles/Home.module.css';
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
